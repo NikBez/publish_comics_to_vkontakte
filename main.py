d1287a8780457851ccff7dafef36bfd5651bd89f
Kit <nikitabit@yandex.ru>
2023-01-28 15:12:32 +0700

    Add Readme Add Requirements Add auto delete

--- main.py
+++ main.py
@@ -1,30 +1,66 @@
+from pathlib import Path
 import requests
 from vk import get_wall_upload_server, post_photo_on_server, save_wall_photo, post_on_the_wall
 from environs import Env
-
-env = Env()
-env.read_env()
-vk_access_token = env('VK_ACCESS_TOKEN')
-group_id = env('GROUP_ID')
-
-url = "https://xkcd.com/353/info.0.json"
-response = requests.get(url)
-response.raise_for_status()
-image_url = response.json()['img']
-alt_text = response.json()['alt']
-
-img = requests.get(image_url)
-response.raise_for_status()
-
-with open("picture.png", "wb") as picture:
-    picture.write(img.content)
-
-upload_address = get_wall_upload_server(vk_access_token, group_id)
-uploaded_photo_params = post_photo_on_server("picture.png", upload_address)
-photo_object = save_wall_photo(vk_access_token, uploaded_photo_params, group_id)
-post_id = post_on_the_wall(vk_access_token, photo_object, group_id, alt_text)
-
-print(post_id)
+from random import randint
+from time import sleep
 
 
+def main():
+
+    env = Env()
+    env.read_env()
+    vk_access_token = env('VK_ACCESS_TOKEN')
+    group_id = env('GROUP_ID')
+
+    while True:
+        try:
+            text = download_random_commix()
+        except requests.exceptions.ConnectionError:
+            print("Connection error. Make new attempt..")
+            sleep(5)
+            continue
+        try:
+            upload_address = get_wall_upload_server(vk_access_token, group_id)
+            uploaded_photo_params = post_photo_on_server("temp_picture.png", upload_address)
+            photo_object = save_wall_photo(vk_access_token, uploaded_photo_params, group_id)
+            post_on_the_wall(vk_access_token, photo_object, group_id, text)
+        except requests.exceptions.ConnectionError:
+            print("Connection error. Make new attempt..")
+            sleep(5)
+            continue
+
+        print("Опубликован новый пост.")
+        Path.unlink("temp_picture.png")
+        break
+
+
+def download_random_commix():
+
+    commix_number = get_random_number()
+
+    image_metadata_url = f"https://xkcd.com/{commix_number}/info.0.json"
+    image_metadata = requests.get(image_metadata_url)
+    image_metadata.raise_for_status()
+    image_metadata = image_metadata.json()
+
+    text = image_metadata['alt']
+
+    img_url = requests.get(image_metadata["img"])
+    img_url.raise_for_status()
+
+    with open("temp_picture.png", "wb") as picture:
+        picture.write(img_url.content)
+    return text
+
+
+def get_random_number():
+    last_commix_url = "https://xkcd.com/info.0.json"
+    response = requests.get(last_commix_url)
+    response.raise_for_status()
+    return randint(1, int(response.json()['num']))
+
+
+if __name__=="__main__":
+    main()
 
--- vk.py
+++ vk.py
@@ -19,9 +19,9 @@ def get_wall_upload_server(vk_access_token, group_id):
             }
 
 
-def post_photo_on_server(photo, upload_params):
+def post_photo_on_server(photo_path, upload_params):
 
-    with open(photo, 'rb') as photo:
+    with open(photo_path, 'rb') as photo:
         url = upload_params["upload_url"]
         files = {
             'photo': photo,
@@ -47,7 +47,7 @@ def save_wall_photo(vk_access_token, uploaded_photo_params, group_id):
         "server": uploaded_photo_params["server"],
         "hash": uploaded_photo_params["hash"],
     }
-    response = requests.get(base_url, headers=header, params=params)
+    response = requests.post(base_url, headers=header, data=params)
     response.raise_for_status()
     response = response.json()
     return {
@@ -61,8 +61,8 @@ def post_on_the_wall(vk_access_token, photo_object, group_id, text=""):
     header = {"Authorization": f"Bearer {vk_access_token}"}
     params = {
         "v": 5.131,
-        "from_group":1,
-        # "group_id": group_id,
+        "owner_id": f'-{group_id}',
+        "from_group": 1,
         "attachments": f"photo{photo_object['owner_id']}_{photo_object['photo_id']}",
         'message': text,
     }
@@ -72,6 +72,18 @@ def post_on_the_wall(vk_access_token, photo_object, group_id, text=""):
     return response
 
 
+# def get_groups(vk_access_token, user_id):
+#     base_url = "https://api.vk.com/method/groups.get"
+#     header = {"Authorization": f"Bearer {vk_access_token}"}
+#     params = {
+#         "v": 5.131,
+#         "user_id": user_id,
+#         # "filter": "admin",
+#     }
+#     response = requests.get(base_url, headers=header, params=params)
+#     response.raise_for_status()
+#     response = response.json()
+#     return response
 
 
 
