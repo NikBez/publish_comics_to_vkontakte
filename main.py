ed13a72be5c74b7dd72b1128ace5f3f531a149e2
Kit <nikitabit@yandex.ru>
2023-01-30 11:43:01 +0700

    Fix bug Remove dicts from "return" Remove unused function Sort imports Create global variable for temp_image Corrected spelling errors ='( Make temp files be cleaned in all situations

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from pathlib import Path
-import requests
-from vk import get_wall_upload_server, post_photo_on_server, save_wall_photo, post_on_the_wall
-from environs import Env
 from random import randint
-from time import sleep
+from pathlib import Path
+import sys
+
+from environs import Env
+import requests
+
+from vk import get_wall_upload_server, post_photo_on_server, save_wall_photo, post_on_the_wall
+
+TEMP_PICTURE_PATH = Path("temp_picture.png")
 
 
 def main():
@@ -11,35 +15,32 @@ def main():
     env = Env()
     env.read_env()
     vk_access_token = env('VK_ACCESS_TOKEN')
-    group_id = env('GROUP_ID')
+    group_id = env('VK_GROUP_ID')
 
-    while True:
-        try:
-            text = download_random_commix()
-        except requests.exceptions.ConnectionError:
-            print("Connection error. Make new attempt..")
-            sleep(5)
-            continue
-        try:
-            upload_address = get_wall_upload_server(vk_access_token, group_id)
-            uploaded_photo_params = post_photo_on_server("temp_picture.png", upload_address)
-            photo_object = save_wall_photo(vk_access_token, uploaded_photo_params, group_id)
-            post_on_the_wall(vk_access_token, photo_object, group_id, text)
-        except requests.exceptions.ConnectionError:
-            print("Connection error. Make new attempt..")
-            sleep(5)
-            continue
+    try:
+        text = download_random_comic()
+    except requests.exceptions.ConnectionError:
+        print("Connection error. Make new attempt..")
+        sys.exit()
+    try:
+        upload_address = get_wall_upload_server(vk_access_token, group_id)
+        server, photo, hash_code = post_photo_on_server(TEMP_PICTURE_PATH, upload_address)
+        owner_id, photo_id = save_wall_photo(vk_access_token, group_id, server, photo, hash_code)
+        post_on_the_wall(vk_access_token, group_id, owner_id, photo_id, text)
+    except requests.exceptions.ConnectionError:
+        print("Connection error. Make new attempt..")
+        sys.exit()
+    finally:
+        Path.unlink(TEMP_PICTURE_PATH)
 
-        print("Опубликован новый пост.")
-        Path.unlink("temp_picture.png")
-        break
+    print("Опубликован новый пост.")
 
 
-def download_random_commix():
+def download_random_comic():
 
-    commix_number = get_random_number()
+    comic_number = get_random_number()
 
-    image_metadata_url = f"https://xkcd.com/{commix_number}/info.0.json"
+    image_metadata_url = f"https://xkcd.com/{comic_number}/info.0.json"
     image_metadata = requests.get(image_metadata_url)
     image_metadata.raise_for_status()
     image_metadata = image_metadata.json()
@@ -55,8 +56,8 @@ def download_random_commix():
 
 
 def get_random_number():
-    last_commix_url = "https://xkcd.com/info.0.json"
-    response = requests.get(last_commix_url)
+    last_comic_url = "https://xkcd.com/info.0.json"
+    response = requests.get(last_comic_url)
     response.raise_for_status()
     return randint(1, int(response.json()['num']))
 
--- vk.py
+++ vk.py
@@ -13,30 +13,22 @@ def get_wall_upload_server(vk_access_token, group_id):
     response.raise_for_status()
     response = response.json()
 
-    return {"album_id": response["response"]["album_id"],
-            "upload_url": response["response"]["upload_url"],
-            "user_id": response["response"]["user_id"]
-            }
+    return str(response["response"]["upload_url"])
 
 
-def post_photo_on_server(photo_path, upload_params):
+def post_photo_on_server(photo_path, upload_url):
 
     with open(photo_path, 'rb') as photo:
-        url = upload_params["upload_url"]
         files = {
             'photo': photo,
         }
-        response = requests.post(url, files=files)
+        response = requests.post(upload_url, files=files)
     response.raise_for_status()
     response = response.json()
-    return {
-        "server": response["server"],
-        "photo": response["photo"],
-        "hash": response["hash"],
-    }
+    return response["server"], response["photo"], response["hash"]
 
 
-def save_wall_photo(vk_access_token, uploaded_photo_params, group_id):
+def save_wall_photo(vk_access_token, group_id, server, photo, hash_code):
 
     base_url = "https://api.vk.com/method/photos.saveWallPhoto"
     header = {"Authorization": f"Bearer {vk_access_token}"}
@@ -44,20 +36,17 @@ def save_wall_photo(vk_access_token, uploaded_photo_params, group_id):
     params = {
         "v": 5.131,
         "group_id": group_id,
-        "photo": uploaded_photo_params["photo"],
-        "server": uploaded_photo_params["server"],
-        "hash": uploaded_photo_params["hash"],
+        "photo": photo,
+        "server": server,
+        "hash": hash_code,
     }
     response = requests.post(base_url, headers=header, data=params)
     response.raise_for_status()
     response = response.json()
-    return {
-        "owner_id": response['response'][0]['owner_id'],
-        "photo_id": response['response'][0]['id']
-    }
+    return response['response'][0]['owner_id'], response['response'][0]['id']
 
 
-def post_on_the_wall(vk_access_token, photo_object, group_id, text=""):
+def post_on_the_wall(vk_access_token, group_id, owner_id, photo_id, text=""):
 
     base_url = "https://api.vk.com/method/wall.post"
     header = {"Authorization": f"Bearer {vk_access_token}"}
@@ -65,22 +54,8 @@ def post_on_the_wall(vk_access_token, photo_object, group_id, text=""):
         "v": 5.131,
         "owner_id": f'-{group_id}',
         "from_group": 1,
-        "attachments": f"photo{photo_object['owner_id']}_{photo_object['photo_id']}",
+        "attachments": f"photo{owner_id}_{photo_id}",
         'message': text,
-    }
-    response = requests.get(base_url, headers=header, params=params)
-    response.raise_for_status()
-    response = response.json()
-    return response
-
-
-def get_groups(vk_access_token, user_id):
-    base_url = "https://api.vk.com/method/groups.get"
-    header = {"Authorization": f"Bearer {vk_access_token}"}
-    params = {
-        "v": 5.131,
-        "user_id": user_id,
-        # "filter": "admin",
     }
     response = requests.get(base_url, headers=header, params=params)
     response.raise_for_status()
